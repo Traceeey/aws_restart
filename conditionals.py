eb95a1e3f739dce0c09655f62786beb430375997
Tracey <145255568+Traceeey@users.noreply.github.com>
2024-10-16 16:59:09 +0300

    Add files via upload

---
+++ conditionals.py
@@ -0,0 +1,11 @@
+userReply = input ("Do you need to ship a package? ")
+if userReply == "stamp":
+    print ("Stamp")
+elif userReply == "envelope":
+    print("Envelope")
+elif userReply == "copy":
+    print("Copy")
+else:
+    print("None")
+
+    
